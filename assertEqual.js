412e0d0bbf69dda0ea0643c8f73a6bfec564c25b
Zoey Farago <zf.zoodle@gmail.com>
2021-01-04 23:43:02 +0000

    lotide node assertion

---
+++ assertEqual.js
@@ -0,0 +1,26 @@
+const pass = function(actual, expected) {
+  return "🤩🥳🤩 Assertion Passed: " + actual + " === " + expected;
+};
+
+const fail = function(actual, expected) {
+  return "❌ Assertion Failed: " + actual + " !== " + expected;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual.isInteger === true) {
+    if (actual = expected) { //eslint error b/c = is used to compare integers instead of ===
+      console.log(pass(actual, expected));
+    } else {
+      console.log(fail(actual, expected));
+    }
+  } else {
+    if (actual === expected) {
+      console.log(pass(actual, expected));
+    } else {
+      console.log(fail(actual, expected));
+    }
+  }
+};
+
+assertEqual("Lighthouse Labs ", "Lighthouse Labs ");
+assertEqual(2, 1);

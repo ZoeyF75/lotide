f12d57b5db6f54d11ac208ce854969aeaf8cc6f0
Zoey Farago <zf.zoodle@gmail.com>
2021-01-06 01:29:41 +0000

    ternary operator/ simplifying function

--- assertEqual.js
+++ assertEqual.js
@@ -5,10 +5,10 @@ const pass = function(actual, expected) {
 const fail = function(actual, expected) {
   return `❌ Assertion Failed: ${actual} !== ${expected}`;
 };
-
+/*
 const assertEqual = function(actual, expected) {
   if (actual.isInteger === true) {
-    if (actual = expected) { //eslint error b/c = is used to compare integers instead of ===
+    if (actual === expected) { //eslint error b/c = is used to compare integers instead of ===
       console.log(pass(actual, expected));
     } else {
       console.log(fail(actual, expected));
@@ -20,6 +20,10 @@ const assertEqual = function(actual, expected) {
       console.log(fail(actual, expected));
     }
   }
+};*/
+
+const assertEqual = (actual, expected, fail) => {
+  actual === expected ? console.log(pass(actual, expected)) : console.log(fail(actual, expected, fail));
 };
 
 assertEqual("Lighthouse Labs ", "Lighthouse Labs ");

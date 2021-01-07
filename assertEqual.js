b2eb95d557b02d7dbeb0bb741de8f13237e0dcab
Zoey Farago <zf.zoodle@gmail.com>
2021-01-07 01:28:05 +0000

    Reafactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,30 +1,8 @@
-const pass = function(actual, expected) {
-  return `🤩🥳🤩 Assertion Passed: ${actual} === ${expected}`;
+const assertEqual = (actual, expected) => {
+  return actual === expected ? `🤩🥳🤩 Assertion Passed: ${actual} === ${expected}` : `❌ Assertion Failed: ${actual} !== ${expected}`;
 };
 
-const fail = function(actual, expected) {
-  return `❌ Assertion Failed: ${actual} !== ${expected}`;
-};
-/*
-const assertEqual = function(actual, expected) {
-  if (actual.isInteger === true) {
-    if (actual === expected) { //eslint error b/c = is used to compare integers instead of ===
-      console.log(pass(actual, expected));
-    } else {
-      console.log(fail(actual, expected));
-    }
-  } else {
-    if (actual === expected) {
-      console.log(pass(actual, expected));
-    } else {
-      console.log(fail(actual, expected));
-    }
-  }
-};*/
-
-const assertEqual = (actual, expected, fail) => {
-  actual === expected ? console.log(pass(actual, expected)) : console.log(fail(actual, expected, fail));
-};
-
-assertEqual("Lighthouse Labs ", "Lighthouse Labs ");
-assertEqual(2, 1);
+console.log(assertEqual("Lighthouse Labs ", "Lighthouse Labs "));
+console.log(assertEqual("hello", "bye"));
+console.log(assertEqual(2,2));
+console.log(assertEqual(2, 1));

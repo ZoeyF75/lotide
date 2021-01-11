fafb333238b6ddb4765f1574d9262db0ac75c091
Zoey Farago <zf.zoodle@gmail.com>
2021-01-11 22:47:16 +0000

    Implimented Mocha and Chai into the test file aand deleted unused code

--- head.js
+++ head.js
@@ -1,13 +1,8 @@
-const assertEqual = require('./assertEqual');
-
 const head = function(array) {
   return array[0];
 };
 
-module.exports = {
-  assertEqual,
-  head
-};
+module.exports = head;
 
 
 
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,20 @@
-const myFuncs = require('../head.js');
-const head = myFuncs.head;
-const assertEqual = myFuncs.assertEqual;
+const assert = require('chai').assert;
+const head = require('../head');
 
-console.log(assertEqual(head([5,6,7]),5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([1]), 1));
-console.log(assertEqual(head([])));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]),1);
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]));
+  });
+});

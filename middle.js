30e808689903dc661d4c0d289cb468cd30dbb420
Zoey Farago <zf.zoodle@gmail.com>
2021-01-11 23:36:25 +0000

    Implementing Mocha and Chai to test middle function

--- middle.js
+++ middle.js
@@ -1,14 +1,9 @@
-const assertArraysEqual = require('./assertArraysEqual.js');
-
 const middle = (midArray) => {
   let mid = [];
-  if (midArray.length <= 2) mid = '[]';
+  if (midArray.length <= 2) mid = [];
   else if (midArray.length % 2 !== 0) mid.push(midArray[Math.floor(midArray.length / 2)]);
   else if (midArray.length % 2 === 0) mid.push(midArray[(midArray.length / 2) - 1],midArray[midArray.length / 2]);
   return mid;
 };
 
-module.exports = {
-  assertArraysEqual,
-  middle
-};
+module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,23 @@
-const myFuncs = require('../middle.js');
-const assertArraysEqual = myFuncs.assertArraysEqual;
-const middle = myFuncs.middle;
+const middle = require('../middle.js');
+const assert = require('chai').assert.deepEqual;
 
-console.log(assertArraysEqual(middle([1]), '[]'));
-console.log(assertArraysEqual(middle([1, 2]), '[]'));
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2,3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]));
+describe("#middle", () => {
+  it("return [] with array of single index", () => {
+    assert(middle([1]), []);
+  });
+  it("return [] with array of only 2 indexes", () => {
+    assert(middle([1,2]), []);
+  });
+  it("return [2] with input of [1, 2, 3]", () => {
+    assert(middle([1, 2, 3]), [2]);
+  });
+  it("return ['hello'] with input of [1, 2, 'hello', 4, 5]", () => {
+    assert(middle([1, 2, 'hello', 4, 5]), ['hello']);
+  });
+  it("return [2,3] with input of [1, 2, 3, 4]", () => {
+    assert(middle([1, 2, 3, 4]), [2,3]);
+  });
+  it("return ['yee','hawe'] with input of [1, 2, 'yee', 'hawe', 5, 6]", () => {
+    assert(middle([1, 2, 'yee', 'hawe', 5, 6]), ['yee','hawe']);
+  });
+});

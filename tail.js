f61d344ab9d85c2bd2a474a2ee0839c2463bfc05
Zoey Farago <zf.zoodle@gmail.com>
2021-01-11 23:13:49 +0000

    Implimented Mocha and Chai assertions into test file and refactoring

--- tail.js
+++ tail.js
@@ -1,11 +1,5 @@
-const assertEqual = require('./assertEqual.js');
-
 const tail = function(array) {
-  return array.length > 1 ? array.slice(1) : '[]';
+  return array.length > 1 ? array.slice(-1) : [];
 };
 
-module.exports = {
-  assertEqual,
-  tail
-};
-
+module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,19 @@
-const myFuncs = require('../tail.js');
-const assertEqual = myFuncs.assertEqual;
-const tail = myFuncs.tail;
+const assert = require('chai').assert;
+const tail = require('../tail.js');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log(assertEqual(words.length, 3));
-console.log(assertEqual(tail(["pineapple"]), '[]'));
-console.log(assertEqual(tail([]), '[]'));
+describe("#tails", () => {
+  it("return 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ["Labs"]);
+  });
+  it("return 5 for length of inputed array (input array is not mutated", () => {
+    const words = ['Fresh', 'Prince', 'Of', 'Bel', 'Air'];
+    tail(words);
+    assert.strictEqual(words.length, 5);
+  });
+  it("return [] when theres only one element in the array", () => {
+    assert.deepEqual(tail(['pineapple']), []);
+  });
+  it("return an empty array when the input array is empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});

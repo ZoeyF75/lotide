cb143b0f7d064dc3ee182edc6e49409b15ffd1b2
Zoey Farago <zf.zoodle@gmail.com>
2021-01-11 21:35:08 +0000

    Refactored assertion function for equal arrays with module exports and created a seperate test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,12 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected){
-    return true;
-  } else {
-    return false;
-  }
+const myFuncs = require('./eqArrays.js');
+const eqArrays = myFuncs.eqArrays;
+
+const assertArraysEqual = (actualArray, expectedArray) => {
+  eqArrays(actualArray, expectedArray) ? console.log(`🤩🥳🤩 Assertion Passed: ${actualArray} === ${expectedArray}`) : console.log(`❌ Assertion Failed: ${actualArray} !== ${expectedArray}`);
+  return eqArrays(actualArray, expectedArray);
 };
 
-const eqArrays = (actualArray, expectedArray) => {
-  let boolean = [];
-  for (let i = actualArray.length - 1; i >= 0; i--){
-    boolean.push(assertEqual(actualArray[i], expectedArray[i]));
-  }
-  eqArraysEqual(actualArray, expectedArray, boolean);
-};
+module.exports = assertArraysEqual;
 
-const eqArraysEqual = (actualArray, expectedArray, boolean) => {
-  for (bool of boolean){
-    if (bool === false){
-      console.log(`❌ Assertion Failed: ${actualArray} !== ${expectedArray}`);
-      return false
-    } else {
-      console.log(`🤩🥳🤩 Assertion Passed: ${actualArray} === ${expectedArray}`);
-      return true;
-    }
-  }
-}
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+

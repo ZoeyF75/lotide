e0a41f61a5e151f89c6d1ac77ccc116c45a808df
Zoey Farago <zf.zoodle@gmail.com>
2021-01-12 05:14:13 +0000

    implementing Mocha and Chai

--- findKey.js
+++ findKey.js
@@ -11,13 +11,3 @@ const findKey = (object, key) => {
 };
 
 module.exports = findKey;
-
-/* Test Code
-console.log(assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2),'noma'));*/
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,12 +6,3 @@ const findKeyByValue = (movieObject, title) => {
 };
 
 module.exports = findKeyByValue;
-
-/*Test Code
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));*/

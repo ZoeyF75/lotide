175c009b87be9d1c34b758af1d88de8d4bdb6ba3
Zoey Farago <zf.zoodle@gmail.com>
2021-01-12 05:16:03 +0000

    Removed unecessary code

--- countLetters.js
+++ countLetters.js
@@ -7,10 +7,3 @@ const countLetters = (string) => {
 };
 
 module.exports = countLetters;
-
-/*Test Code
-const example = 'pineapple';
-const comparison = countLetters(example);
-console.log(countLetters("hello i am a string"));
-console.log(assertEqual(comparison['p'], 3));
-console.log(assertEqual(comparison['e'], 2));*/
